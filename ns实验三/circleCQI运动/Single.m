d3c5c4b63c9d89ca068cc3c6c58ed56b60ec04f3
dragon-liu <liuzhichao@hust.edu.cn>
2019-07-19 11:40:43 +0800

    7.19

--- ns实验三/circleCQI运动/Single.m
+++ ns实验三/circleCQI运动/Single.m
@@ -1,18 +1,21 @@
-data = csvread('CQI4_1_1800.csv'); % 如果不在工作目录，那就写完整的路径
+data = csvread('5_1_600.csv'); % 如果不在工作目录，那就写完整的路径
 x=data(:,1);%x轴上的数据
 y1=data(:,2); %a数据y值
 y2=data(:,3); %a数据y值
 y3=data(:,4); %a数据y值
 y4=data(:,5); %a数据y值
 plot(x,y1,'-*r',x,y2,'-og',x,y3,'-+b',x,y4,'-xc'); %线性，颜色，标记
-axis([0,30,0,10])  %确定x轴与y轴框图大小
+axis([0,30,10,15])  %确定x轴与y轴框图大小
 set(gca,'XTick',[0:0.5:30]) 
-set(gca,'YTick',[0:1:10]) 
+set(gca,'YTick',[10:1:15]) 
 set(gcf,'DefaultAxesFontName','times');
 set(gcf,'DefaultAxesFontSize',14);
 legend('UE1','UE2','UE3','UE4');
-name='CQI-1800m-PF';
+name='CQI-600m-MR';
 title(name);   %右上角标注
 xlabel('time/s')  %x轴坐标描述
 ylabel('CQI') %y轴坐标描述
 print(gcf,'-dpng',name);
+
+
+
---
+++ ns实验三/circleCQI运动/getcqi.py
@@ -0,0 +1,19 @@
+import re
+
+pat = re.compile(
+    r'(.*?)ns UE:(\d+)at.*?CQI:(\d+)current throughput(.*?)avg throughput(.*?)$')
+info = [[], [], [], [], []]
+with open('5_1_600', 'r') as f:
+    for line in f.readlines():
+        mat = pat.match(line)
+        if mat:
+            info[int(mat.group(2))].append((float(mat.group(1))/1e6,
+                                            int(mat.group(3)), float(mat.group(4))))
+
+for u in range(1, 5):
+    tm = 0
+    print('UE ', u)
+    for t, c, p in info[u]:
+        if t > tm:
+            tm += 0.5
+            print(t, c)
